018ee768808a847f51f980706e376a98e54fb217
Yasmim <yasmimoliveira.ad@gmail.com>
2023-10-16 14:07:28 -0300

    initial commit

---
+++ src/main/java/br/com/yasmimsilva/todolist/user/UserController.java
@@ -0,0 +1,34 @@
+package br.com.yasmimsilva.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(400).body("Usuário já existe");
+        }
+
+        var passHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(200).body(userCreated);
+
+    }
+}
---
+++ src/main/java/br/com/yasmimsilva/todolist/utils/utils.java
@@ -0,0 +1,32 @@
+package br.com.yasmimsilva.todolist.utils;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class utils {
+
+    public static void copyNonNullProperties(Object source, Object target) {
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<>();
+
+        for (PropertyDescriptor pd : pds) {
+            Object scrValue = src.getPropertyValue(pd.getName());
+            if (scrValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+}
